c95d5642d218c88b0800044061ff8ffff57764e1
lais-cardoso <lalaicardoso27@hotmail.com>
2024-03-17 21:24:39 -0300

    feat: add register form

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import './styles.css'
 import axios from 'axios'
+import RecodeLogo from '../assets/recode-jr-logo.png'
 
 function Form() {
 
@@ -12,73 +13,91 @@ function Form() {
         axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/municipios').then((response) => {
             setCidades(response.data)
         }).catch(error => {
-            if(error.response !== undefined){
+            if (error.response !== undefined) {
                 console.log('A api está com problemas!')
-            }else{
+            } else {
                 console.log('Error!')
             }
         })
-    },[])
+    }, [])
 
     useEffect(() => {
         axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then((response) => {
             setEstados(response.data)
         }).catch(error => {
-            if(error.response !== undefined){
+            if (error.response !== undefined) {
                 console.log('A api está com problemas!')
-            }else{
+            } else {
                 console.log('Error!')
             }
         })
-    },[])
+    }, [])
 
     useEffect(() => {
         axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/paises').then((response) => {
             setPaises(response.data)
         }).catch(error => {
-            if(error.response !== undefined){
+            if (error.response !== undefined) {
                 console.log('A api está com problemas!')
-            }else{
+            } else {
                 console.log('Error!')
             }
         })
-    },[])
+    }, [])
     return (
-        <form>
-            <h1>Formulário de Cadastro</h1>
-            <h5>Seja bem vindo ao nosso site!</h5>
+        <div className="container">
+            <div className="elements">
+                <div className="logo-container">
+                    <img src={RecodeLogo} alt="logo-recode-jr" />
+                </div>
+                <form>
+                    <div className="title">
+                        <h1>Crie a sua conta</h1>
+                        <h5>Preencha o formulário</h5>
+                    </div>
+                    <div className="form-fields">
+                        <p>Nome</p>
+                        <input className="input" type="text"></input>
 
-            <p>Nome</p>
-            <input type="text"></input>
+                        <p>E-mail</p>
+                        <input className="input" type="text"></input>
 
-            <p>E-mail</p>
-            <input type="text"></input>
+                        <div className="state-country">
+                            <div>
+                                <p>País</p>
+                                <select className="input">
+                                    {paises.map((item, index) => {
+                                        return <option value={index} key={index}>{item.nome}</option>
+                                    })}
+                                </select>
+                            </div>
 
-            <p>Município</p>
-            <select>
-                {cidades.map((item, index) => {
-                   return  <option value={index} key={index}>{item.nome}</option>
-                })}
-            </select>
+                            <div>
+                                <p>Estado</p>
+                                <select className="input">
+                                    {estados.map((item, index) => {
+                                        return <option value={index} key={index}>{item.nome}</option>
+                                    })}
 
-            <p>Estado</p>
-            <select>
-                {estados.map((item, index) => {
-                    return <option value={index} key={index}>{item.nome}</option>
-                })}
-                
-            </select>
+                                </select>
+                            </div>
 
-            <p>País</p>
-            <select>
-                {paises.map((item, index) => {
-                   return <option value={index} key={index}>{item.nome}</option>
-                })}
-            </select>
 
-            <button>Enviar</button>
+                        </div>
 
-        </form>
+                        <p>Município</p>
+                        <select className="input">
+                            {cidades.map((item, index) => {
+                                return <option value={index} key={index}>{item.nome}</option>
+                            })}
+                        </select>
+                    </div>
+
+                    <button>Enviar</button>
+
+                </form>
+            </div>
+        </div>
     )
 }
 
